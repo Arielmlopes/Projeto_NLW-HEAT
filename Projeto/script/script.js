e239cd87a42e2907cc4e428bce38086af798cf95
Ariel Machado Lopes <arielmachadolopes@gmail.com>
2021-10-25 23:15:55 -0300

    final aula 03 Nlw

--- Projeto/script/script.js
+++ Projeto/script/script.js
@@ -1,5 +1,20 @@
-let name = "ariel machado lopes";
 
-//  name = 'Ariel Lopes';
+//criando um objeto com cada link das minhas redes sociais
+ const linksRedeSocial = {
+     github: "Arielmlopes",
+     instagram: "arielmlopes",
+     facebook: "arielmlopes"
+    
+ }
 
- 
+ function mudandoLinksRedeSocial(){
+      for( let li of socialLinks.children){
+          const social = li.getAtribute('class')
+        li.children[0].href = `https://${social}.com/${linksRedeSocial[social]}`
+
+      }
+ }
+
+//  mudandoLinksRedeSocial()
+
+   
