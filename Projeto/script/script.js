cba82992147a27ef7a97b3f677db08ca3cee17e9
Ariel Machado Lopes <arielmachadolopes@gmail.com>
2021-10-27 00:35:29 -0300

    add animation card 3d

--- Projeto/script/script.js
+++ Projeto/script/script.js
@@ -28,3 +28,12 @@
        })
    }
    getGithubInfos()
+
+
+   VanillaTilt.init(document.querySelector(".box"), {
+		max: 25,
+		speed: 400
+	});
+	
+	//It also supports NodeList
+	VanillaTilt.init(document.querySelectorAll(".box"));
